afb37143ec236f366689b018d77b6febc37750f5
Youssif Elsayed <107424809+arrayunderscore@users.noreply.github.com>
2024-07-21 15:35:51 +0300

    update

--- carfigures/packages/battle/battle_user.py
+++ carfigures/packages/battle/battle_user.py
@@ -4,22 +4,22 @@ from typing import TYPE_CHECKING
 if TYPE_CHECKING:
     import discord
 
-    from ballsdex.core.bot import BallsDexBot
-    from ballsdex.core.models import BallInstance, Player
-    from ballsdex.core.models import Trade as Battle
+    from carfigures.core.bot import CarFiguresBot
+    from carfigures.core.models import CarInstance, Player
+    from carfigures.core.models import Trade as Battle
 
 
 @dataclass(slots=True)
 class BattlingUser:
     user: "discord.User | discord.Member"
     player: "Player"
-    proposal: list["BallInstance"] = field(default_factory=list)
+    proposal: list["CarInstance"] = field(default_factory=list)
     locked: bool = False
     cancelled: bool = False
     accepted: bool = False
 
     @classmethod
-    async def from_battle_model(cls, battle: "Battle", player: "Player", bot: "BallsDexBot"):
-        proposal = await battle.tradeobjects.filter(player=player).prefetch_related("ballinstance")
+    async def from_battle_model(cls, battle: "Battle", player: "Player", bot: "CarFiguresBot"):
+        proposal = await battle.tradeobjects.filter(player=player).prefetch_related("carinstance")
         user = await bot.fetch_user(player.discord_id)
-        return cls(user, player, [x.ballinstance for x in proposal])
+        return cls(user, player, [x.carinstance for x in proposal])
